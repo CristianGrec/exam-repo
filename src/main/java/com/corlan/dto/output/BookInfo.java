e04d84e2e4fd04e32df3297d0cc91b0cdee9a3bc
Cristian Grec <cristian.grec@savvymoney.com>
2023-01-22 10:17:18 +0200

    added dto

---
+++ src/main/java/com/corlan/dto/output/BookInfo.java
@@ -0,0 +1,21 @@
+package com.corlan.dto.output;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class BookInfo implements Serializable {
+    public String title;
+    public List<String> authors;
+    public String publishedDate;
+    public List<IndustryIdentifiers> industryIdentifiers;
+    public String pageCount;
+    public String language;
+
+}
